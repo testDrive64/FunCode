650bcc77ad70f9493193c33e25ce378ff700167e
testDrive64 <com9on@me.com>
2016-02-13 23:37:44 +0100

    Little changes and new testProg

--- accountMenu.c
+++ accountMenu.c
@@ -2,56 +2,137 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 30
+
+struct personDetails {
+  char vname[MAX];
+  char nname[MAX];
+  long PLZ;
+  char ort[MAX];
+  int geburtsjahr;
+  int used;
+};
+
+typedef struct personDetails Person;
+
+//--------- Functions ---------//
+Person newAccount();
+Person editAccount(Person);
+Person deleteAccount(Person);
+void showList(Person[]);
+
+
 int main(int argc, char *argv[]) {
   int change = 0;
+  int i = 0;
+  int running = 0;
+  char firstname[MAX];
 
-  printf(
-    "Menu\n
-     1. New Account\n
-     2. Change Account\n
-     3. Delete Account\n
-     4. Show all Accounts\n
-     5. Quit\n
-     Press the Number and enter to change the Menu.\n"
-  );
-
-  scanf(change);
-
-  switch(change) {
-    case 1:
-      newAccount();
-      break;
-    case 2:
-      changeAccount();
-      break;
-    case 3:
-      deleteAccount();
-      break;
-    case 4:
-      showList();
-      break;
-    case 5:
-      return 0;
-      break;
-    default:
-      printf("Wrong menu input!");
-      break;
+  Person personList[100];
+  while (i<100) {
+    // First Declaration of the personlist is
+    // to deaktivate every person in the list.
+    personList[i].used = 0;
+    i++;
   }
+
+  Person bufferPers;
+  i=0;
+  //------------- Menu -------------//
+
+  while(running == 0) {
+    printf(
+      "Menu\n \
+       1. New Account\n \
+       2. Edit Account\n \
+       3. Delete Account\n \
+       4. Show all Accounts\n \
+       5. Quit\n \
+       Press the Number and enter to change the Menu.\n"
+    );
+
+    scanf("%i", &change);
+
+    switch(change) {
+      case 1:
+
+        while (personList[i].used != 0) {
+          i++;
+        }
+        personList[i] = newAccount();
+        printf("Person %s %s is saved to the List on number %i.\n", personList[i].vname, personList[i].nname, i+1);
+        i=0;
+        break;
+
+      case 2:
+
+        printf("Edit Account\n");
+        printf("Which Account you want to change(Firstname): ");
+        fgets(firstname, MAX, stdin);
+
+        for (int i=0; (personList[i].vname == firstname); i++){}
+        personList[i] = editAccount(personList[i]);
+
+        break;
+
+      case 3:
+        personList[i] = deleteAccount(personList[i]);
+        break;
+      case 4:
+        showList(personList);
+        break;
+      case 5:
+        running = 1;
+        return 0;
+        break;
+      default:
+        printf("Wrong menu input!");
+        break;
+      }
+    }
 }
 
-/*
- * Hier geht es weiter, alle Menü punkte mit Code auffüllen
- * und testen.
- * So Variabel wie möglich aufbauen, damit man den Code für jegliche
- * anderen Objekte einfach umschreiben kann.
- *
- *  Objekt (Mensch) Struct am Anfang.
-*/
 
-void newAccount() {}
+Person newAccount() {
+  Person pers;
+  pers.vname[0] = "";
+  char buffer[2];
+  fgets(buffer, 2, stdin);
 
-void changeAccount() {}
+  printf("Neue Person anlegen\n");
 
-void deleteAccount() {}
+  printf("Vorname      : ");
+  fgets(pers.vname, MAX, stdin);
+  printf("Nachname     : ");
+  fgets(pers.nname, MAX, stdin);
 
-void showList() {}
+  printf("Postleitzahl : ");
+  do {
+    scanf("%51d", &pers.PLZ);
+  } while(getchar() != '\n');
+
+  printf("Wohnort      : ");
+  fgets(pers.ort, MAX, stdin);
+
+  printf("Geburtsjahr  : ");
+  do {
+    scanf("%4d", &pers.geburtsjahr);
+  } while(getchar() != '\n');
+
+  pers.used=1;
+
+  return pers;
+}
+
+Person editAccount(Person pers) {
+  return pers;
+}
+
+Person deleteAccount(Person pers) {
+  pers.used = 0;
+  return pers;
+}
+
+void showList(Person *list) {
+
+}
--- passGen.c
+++ passGen.c
@@ -8,20 +8,38 @@
 // 4. Zufallszahlen und Zufallsbuchstaben in Zufälliger
 //	Reihenfolge zusammen schreiben
 
+char bigLetterArr[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+						 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+char smallLetterArr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+						   'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+char randomCharacter(int);
 
 int main() {
 	int randInt;
 	char randChar;
-	char pass[25];
+	char pass[26];
+
+	for(int j=0;j<27;j++) {
+		printf("Small letter: %c\n", smallLetterArr[j]);
+		printf("Big letter: %c\n", bigLetterArr[j]);
+	}
 
 	srand(time(NULL));
 
-	for (int i=0; i < 25; i++) {
+	for (int i=0; i < 27; i+=2) {
 		randInt = rand() % 50;
 		randChar = rand() % 25;
 		printf("The number is: %d\n", randInt);
-		pass[i] = (char)randInt;
+		pass[i] = randInt;
+		pass[i+1] = randomCharacter(randChar);
 		printf("Pass: %d -> %d\n", i, pass[i]);
+	}
+
+	printf("The generated password: \n\t\t ");
+	for (int i=0; i<27; i++) {
+		printf("%c", pass[i]);
 	}
 
 	return 0;
@@ -30,10 +48,10 @@ int main() {
 char randomCharacter(int count) {
 	char letter;
 
-	
-
-	letter = 
-	
+	if ((count %2) == 0) {
+		letter = bigLetterArr[count-1];
+	} else {
+		letter = smallLetterArr[count-1];
+	}
 	return letter;
 }
-
