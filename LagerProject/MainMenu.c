fb9c42e792cf2e472958f3b01254e2feb2e52063
testDrive64 <com9on@me.com>
2016-06-07 16:35:50 +0200

    update repository

---
+++ LagerProject/MainMenu.c
@@ -0,0 +1,13 @@
+#include <stdio>
+#include <stdlib>
+
+int main() {
+	printf("*header*Hauptmenu Lager Logistic");
+
+	printf("1. Database   anlegen.");
+	printf("2. Databases  anzeigen.");
+	printf("3. Use Database");
+	printf("4. Delete Database");
+
+	printf("");
+}
