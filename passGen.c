56b02b08ff7f259607ac4ce92c0e0989999c9fb3
testDrive64 <com9on@me.com>
2016-06-07 15:42:14 +0200

    Passwort generator

--- passGen.c
+++ passGen.c
@@ -2,38 +2,111 @@
 #include <time.h>
 #include <stdlib.h>
 
-// 1. Zufallszahlen Int (0-50)
-// 2. Zufallszahlen Char (0-25)
-// 3. Char Zufallszahl in Buchstaben umwandeln
-// 4. Zufallszahlen und Zufallsbuchstaben in Zufälliger
-//	Reihenfolge zusammen schreiben
 
+char intToCharacter(int);
+void controller(int*, int*, int*, int*, int);
+void passGenerator(int*, char*, char*);
 
 int main() {
-	int randInt;
-	char randChar;
-	char pass[25];
 
-	srand(time(NULL));
+  int intBuffer = 0;
+  int charBuffer = 0;
 
-	for (int i=0; i < 25; i++) {
-		randInt = rand() % 50;
-		randChar = rand() % 25;
-		printf("The number is: %d\n", randInt);
-		pass[i] = (char)randInt;
-		printf("Pass: %d -> %d\n", i, pass[i]);
-	}
+  int  randInt; 
+  int  randChar;
+  int  pass[25];
+  int  charBuff[25];
+  char passChar[25];
 
-	return 0;
+  char passWord[54];
+
+  srand(time(NULL));
+
+  for (int i=0; i < 25; i++) {
+    randInt = rand() % 50;
+    randChar = rand() % 25;
+
+    controller(&randInt, &randChar, &pass[0], &charBuff[0], i);
+
+    charBuff[i] = randChar;
+    passChar[i] = (char)intToCharacter(randChar);
+    pass[i] = randInt;
+    printf("The number is %i\n", pass[i]);
+    printf("The character is: %c\n", passChar[i]);
+  }
+
+  for (int i=0; i< 25; i++) {
+    printf("%i -> [%i,  %c]\n", i, pass[i], passChar[i]);
+  }
+
+  passGenerator(&pass[0], &passChar[0], &passWord[0]);
+  printf("Passwort: ");
+  for (int i=0; i < 54; i++) {
+    printf("%c", passWord[i]);
+  }
+
+  return 0;
 }
 
-char randomCharacter(int count) {
-	char letter;
+char intToCharacter(int charNum) {
+  char letter;
 
-	
+  int asciiNum = 65 + charNum%24;
+  int asciiSmallNum = 97 + charNum%24;
 
-	letter = 
-	
-	return letter;
+  letter = (char)asciiNum;
+
+  return letter;
+}
+
+void controller(int *rI, int *rC, int *pA, int *pCA, i) {
+
+  /*********************Testoutput********************/
+
+  printf("\nrI  is %i -> %i\n", rI, *rI);
+  printf("\nrC  is %i -> %i\n", rC, *rC);
+  printf("\npA  is %i -> %i\n", pA, *pA);
+  printf("\npCA is %i -> %i\n", pCA, *pCA);
+
+  /***************************************************/
+
+  for (int j = 0; j < i; j++) {
+    while (*rI == *pA) {
+      printf("Found a double int\n");
+      *rI = rand() % 50;
+    }
+    while (*rC == *pCA) {
+      printf("Found a double char\n");
+      *rC = rand() % 25;
+    }
+
+    pA++;
+    pCA++;
+  }
+}
+
+char intToAscii(int num) {
+  char res = (char)num%9 +48;
+  return res;
+}
+
+void passGenerator(int *pA, char *pCA, char *res) {
+
+    for (int i=1; i<55; i++) {
+      if ((i%5) == 0) {
+        *res = '_';
+        printf("1. res = %c\n", *res);
+      } else if ((i%2) == 0) {
+        *res = intToAscii(*pA);
+        pA++;
+        printf("2. res = %c\n", *res);
+      } else {
+        *res = *pCA;
+        pCA++;
+        printf("3. res = %c\n", *res);
+      }
+      printf("i = %i\n", i);
+      res++;
+    }
 }
 
