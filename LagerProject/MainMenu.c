db15ae7bb2c5098295edfddaf848c54a03908212
testDrive64 <com9on@me.com>
2016-06-11 15:25:08 +0200

    add some functionality

--- LagerProject/MainMenu.c
+++ LagerProject/MainMenu.c
@@ -1,13 +1,113 @@
-#include <stdio>
-#include <stdlib>
+#include <stdio.h>
+#include <stdlib.h>
+
+void newDatabase();
+void showDatabase(int);
+void useDatabase(int);
+void deleteDatabase(int);
 
 int main() {
-	printf("*header*Hauptmenu Lager Logistic");
+  int choose;
+  int db = 0;
 
-	printf("1. Database   anlegen.");
-	printf("2. Databases  anzeigen.");
-	printf("3. Use Database");
-	printf("4. Delete Database");
+  printf("Hauptmenu Lager Logistic\n\n");
 
-	printf("");
+  printf("1. New    Database.\n");
+  printf("2. Show   Database.\n");
+  printf("3. Use    Database.\n");
+  printf("4. Delete Database.\n");
+
+  printf("-----------------------\n");
+  printf("Choose a menu: ");
+  scanf("%i", &choose);
+  printf("\n");
+
+  switch (choose) {
+    case 1:
+      printf("You want to make a new database.\n");
+      newDatabase();
+      break;
+    case 2:
+      printf("You want to show a database.\n");
+      printf("Which Database you want to see (id or name): ");
+      scanf("%i", &db);
+      if (db != 0)
+        showDatabase(db);
+      else
+        printf("\n\n--! Database does not exist !--\n\n");
+      db = 0;
+      break;
+    case 3:
+      printf("You want to edit a database.\n");
+      printf("Which Database you want to edit (id or name): ");
+      scanf("%i", &db);
+      if (db != 0)
+        editDatabase(db);
+      else
+        printf("\n\n--! Database does not exist !--\n\n");
+      db = 0;
+      break;
+    case 4:
+      printf("You want to delete a database.\n");
+      printf("Which Database you want to delete (id or name): ");
+      scanf("%i", &db);
+      if (db != 0)
+        deleteDatabase(db);
+      else
+        printf("\n\n--! Database does not exist !--\n\n");
+      db = 0;
+      break;
+    }
+
+  return 0;
 }
+
+void newDatabase() {
+
+  char choiceYN = 'y';
+  char *fileName;
+
+  printf("\n------- New Database -------\n");
+  printf("\nYou want to make a new Database (y/n): ");
+  scanf(" %c", &choiceYN);
+  getchar();
+  printf("\n");
+
+  if (choiceYN != 'y'){
+    exit(1);
+  }
+
+  printf("What is the name of the database: ");
+
+  do{
+  scanf(" %c", &fileName);
+  } while(getchar() != '\n');
+
+  FILE *db = fopen(("%s.json", fileName), "w+");
+  if (NULL == db)
+    printf("ERROR Datei wurde nicht erstellt!");
+
+}
+
+void showDatabase(int db) {
+
+
+}
+
+
+void useDatabase(int db) {
+
+
+
+}
+
+void editDatabase(int db) {
+
+
+}
+
+void deleteDatabase(int db) {
+
+
+}
+
